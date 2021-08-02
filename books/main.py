9bd01f6620615fde64e42c473a6cb9b503eb8b9e
Arnon Moscona <arnon.moscona@syapse.com>
2021-08-02 18:18:29 -0400

    Added a home page

--- books/main.py
+++ books/main.py
@@ -1,14 +1,17 @@
 """Main module for the web app"""
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 
 from books.books_db import load_books_as_dicts
-from books.resources.resource import static_path
+from books.resources.resource import static_path, get_html
 from books.routes.api_routes import api_router
 from books.routes.ui_routes import ui_router
 
 app = FastAPI(title='Fake book DB', openapi_url='/openapi.json')
 app.mount('/ui/assets', StaticFiles(directory=f'{static_path()}/assets'))
+app.mount('/design/assets', StaticFiles(directory=f'{static_path()}/assets'))
+app.mount('/assets', StaticFiles(directory=f'{static_path()}/assets'))
 
 
 app.include_router(api_router)
@@ -20,6 +23,11 @@ async def startup_event():
     print('Initializing...')
     load_books_as_dicts()
     print('Ready.')
+
+
+@app.get('/', status_code=200, response_class=HTMLResponse)
+async def root():
+    return get_html('index')
 
 
 if __name__ == "__main__":
